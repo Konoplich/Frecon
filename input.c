b6f83f4356537d244000f00ad730759474584633
David Sodman <dsodman@chromium.org>
2014-12-03 10:05:19 -0800

    Only report user activity on key-down and key-repeat
    This change ensures that user activity is not reported on
    key-up events.  The user-activity report was causing the
    power manager to bump the display brightness slightly preventing
    the brightness from ever staying at 0.
    
    BUG=chromium:434123
    TEST=test on link_freon, verify can shutoff screen via brightness keys
    
    Change-Id: If1e83f59a7a7b66bd82c323915c7cd050523651e

--- input.c
+++ input.c
@@ -468,7 +468,6 @@ struct input_key_event *input_get_event(fd_set * read_set,
 				    malloc(sizeof (*event));
 				event->code = ev.code;
 				event->value = ev.value;
-				report_user_activity(USER_ACTIVITY_OTHER);
 				return event;
 			}
 		}
@@ -518,6 +517,9 @@ int input_run(bool standalone)
 			if (!input_special_key(event) && event->value) {
 				uint32_t keysym, unicode;
 				if (term_is_active(terminal)) {
+					// Only report user activity on key-down and key-repeat
+					if (event->value)
+						report_user_activity(USER_ACTIVITY_OTHER);
 					input_get_keysym_and_unicode(
 						event, &keysym, &unicode);
 					term_key_event(terminal,
