Konoplich/Frecon

commit 35d6bd8ce242296737b3fb68c62845bed233c6cd

Ensure frecon holds drmMaster until it's switching away
This change ensures frecon keeps hold of drmMaster until
it is explicitly switching away to let another process take
master.   Releasing too soon allows full-screen chrome to
take master and draw while the console should be shown.

BUG=chromium:435750
TEST=test with full-screen (and non-full-screen) youtube and webgl

Change-Id: I99e6e055d6eab12dea4301e99e21f7b299bb188d
Reviewed-on: https://chromium-review.googlesource.com/231662
Reviewed-by: Dominik Behr <dbehr@chromium.org>
Commit-Queue: David Sodman <dsodman@chromium.org>
Tested-by: David Sodman <dsodman@chromium.org>

splash.c
@@ -367,7 +367,9 @@ int splash_run(splash_t* splash, dbus_t** dbus)
 		}
 	}
 
+
 	/* Let chrome know it's ok to take drmMaster */
+	video_release(splash->video);
 	(void)dbus_method_call0(splash->dbus,
 		kLibCrosServiceName,
 		kLibCrosServicePath,

input.c
@@ -156,6 +156,7 @@ static int input_special_key(struct input_key_event *ev)
 			case KEY_F1:
 				input_ungrab();
 				input.terminals[input.current_terminal]->active = false;
+				video_release(input.terminals[input.current_terminal]->video);
 				(void)dbus_method_call0(input.dbus,
 					kLibCrosServiceName,
 					kLibCrosServicePath,

video.c
@@ -396,9 +396,13 @@ int32_t video_setmode(video_t* video)
 		LOG(ERROR, "Unable to hide cursor");
 
 done:
-	drmDropMaster(video->fd);
 
 	return ret;
+}
+
+void video_release(video_t* video)
+{
+	drmDropMaster(video->fd);
 }
 
 void video_close(video_t *video)

video.h
@@ -58,6 +58,7 @@ int32_t video_getheight(video_t* video);
 int32_t video_getpitch(video_t* video);
 int32_t video_getscaling(video_t* video);
 int32_t video_setmode(video_t* video);
+void video_release(video_t* video);
 bool video_set_gamma(video_t* video, const char *filename);
 void video_close(video_t*);
 uint32_t* video_lock(video_t* video);

commit 5a8013969ec9a02f5ab7556e40695261d630035b

Release master from splash screen right after setmode
This allows chrome to take master as soon as it starts.  This
was regressed in change 35d6bd8ce242296737b3fb68c62845bed233c6cd
which caused frecon to hold master until the user pressed
ctrl-alt-f1

BUG=chromium:440088
TEST=boot with link_freon and verify fix via chrome log file
TEST=verify full-screen video works with console

Change-Id: Ice0cdc93084674137efce77cde50480bbef774a0

splash.c
@@ -253,6 +253,11 @@ static void splash_clear_screen(splash_t *splash, uint32_t *video_buffer)
 
 	video_setmode(splash->video);
 
+	/* After the mode is set, there is nothing splash
+	 * needs master for
+	 */
+	video_release(splash->video);
+
 	bp = video_get_buffer_properties(splash->video);
 
 		for (j = 0; j < bp->height; j++) {
@@ -368,8 +373,6 @@ int splash_run(splash_t* splash, dbus_t** dbus)
 	}
 
 
-	/* Let chrome know it's ok to take drmMaster */
-	video_release(splash->video);
 	(void)dbus_method_call0(splash->dbus,
 		kLibCrosServiceName,
 		kLibCrosServicePath,