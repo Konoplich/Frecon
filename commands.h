604e5a315b1597213daf38b0c850ee39a0a7a382
David Sodman <dsodman@chromium.org>
2015-01-06 09:23:40 -0800

    Add API to support create/switch VT's
    1. Add a an API (as described in the frecon design document) to
    allow an external program to request frecon create a terminal
    and switch the screen to a specific terminal.  This is intended
    to mimic the existing usage of openvt and chvt
    
    2. Add support use sockets rather than dbus for this API.   This
    is necessary for recovery mode which does not run dbus
    
    BUG=chrome-os-partner:36162
    TEST=test program using sockets and dbus on link_freon
    
    Change-Id: Ice37fde07b6607be6f73aa680c31cbc1d8e9a8d0

---
+++ commands.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#define COMMAND_MAKE_VT               "MakeVT"
+#define COMMAND_SWITCH_VT             "SwitchVT"
+
+#endif
--- dbus.h
+++ dbus.h
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Copyright 2014 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
-#ifndef DBUS_H_
-#define DBUS_H_
+#ifndef FRECON_DBUS_H
+#define FRECON_DBUS_H
 
 #include <dbus/dbus.h>
 #include <stdbool.h>
@@ -14,6 +14,8 @@
 
 #define DBUS_STATUS_NOERROR     (0)
 #define DBUS_STATUS_TIMEOUT     (-1)
+
+#define DBUS_FRECON_INTERFACE   "org.chromium.frecon"
 
 typedef struct _dbus_t dbus_t;
 
@@ -34,9 +36,10 @@ bool dbus_signal_match_handler(
     const char* rule,
     dbus_message_handler_t handler,
     void *user_data);
-int dbus_wait_for_messages(dbus_t *dbus, int64_t timeout_ms);
-void dbus_stop_wait(dbus_t* dbus);
 void dbus_destroy(dbus_t* dbus);
+void dbus_add_fd(dbus_t* dbus, fd_set* read_set, fd_set* exception_set);
+int dbus_get_fd(dbus_t* dbus);
+void dbus_dispatch_io(dbus_t* dbus);
 
 
-#endif // DBUS_H_
+#endif // FRECON_DBUS_H
