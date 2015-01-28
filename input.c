Konoplich/Frecon

commit 944073f38d0c2a05a4c32bafd672ba1d65724c4d

Ensure terminal is active before trying to process input events
In the input loop, before attempting to obtain input events and
process them, make sure that the terminal is active.

BUG=chromium:444941
TEST=test on link_freon

Change-Id: I425a1e6dbb58ff20e503ce7ba33e88366c8a7fe8
Reviewed-on: https://chromium-review.googlesource.com/242085
Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
Commit-Queue: David Sodman <dsodman@chromium.org>
Trybot-Ready: David Sodman <dsodman@chromium.org>
Tested-by: David Sodman <dsodman@chromium.org>

input.c
@@ -500,24 +500,28 @@ int input_run(bool standalone)
 		if (term_exception(terminal, &exception_set))
 			return -1;
 
-		struct input_key_event *event;
-		event = input_get_event(&read_set, &exception_set);
-		if (event) {
-			if (!input_special_key(event) && event->value) {
-				uint32_t keysym, unicode;
-				if (term_is_active(terminal)) {
-					// Only report user activity when the terminal is active
-					report_user_activity(USER_ACTIVITY_OTHER);
-					input_get_keysym_and_unicode(
-						event, &keysym, &unicode);
-					term_key_event(terminal,
-							keysym, unicode);
+		if (term_is_active(terminal)) {
+			struct input_key_event *event;
+			event = input_get_event(&read_set, &exception_set);
+			if (event) {
+				if (!input_special_key(event) && event->value) {
+					uint32_t keysym, unicode;
+					// current_terminal can possibly change during
+					// execution of input_special_key
+					terminal = input.terminals[input.current_terminal];
+					if (term_is_active(terminal)) {
+						// Only report user activity when the terminal is active
+						report_user_activity(USER_ACTIVITY_OTHER);
+						input_get_keysym_and_unicode(
+							event, &keysym, &unicode);
+						term_key_event(terminal,
+								keysym, unicode);
+					}
 				}
+
+				input_put_event(event);
 			}
-
-			input_put_event(event);
 		}
-
 
 		term_dispatch_io(terminal, &read_set);
 

commit e49f3b59eb9152b625bdf9264f16f70e4269af3a

Fix input_special_key bypass when no terminal is active
Change 944073f38d0c2a05a4c32bafd672ba1d65724c4d introduced
a regression in that it only calls input_special_key if there
is an active terminal, but, the first time one presses
ctrl-alt-f2 to get an active terminal, there is no terminal
active so it skips input_special_key and therefore has no way
of creating a terminal.

BUG=chromium:452696
TEST=test on link_freon

Change-Id: I07b1768a491aecc455e2a3d088ff1fc562d884de

input.c
@@ -101,6 +101,8 @@ static int input_special_key(struct input_key_event *ev)
 		BTN_TASK
 	};
 
+	terminal = input.terminals[input.current_terminal];
+
 	for (i = 0; i < ARRAY_SIZE(ignore_keys); i++)
 		if (ev->code == ignore_keys[i])
 			return 1;
@@ -123,37 +125,38 @@ static int input_special_key(struct input_key_event *ev)
 		return 1;
 	}
 
-	if (input.kbd_state.shift_state && ev->value) {
-		switch (ev->code) {
-		case KEY_PAGEUP:
-			term_page_up(input.terminals[input.current_terminal]);
-			return 1;
-		case KEY_PAGEDOWN:
-			term_page_down(input.terminals[input.current_terminal]);
-			return 1;
-		case KEY_UP:
-			term_line_up(input.terminals[input.current_terminal]);
-			return 1;
-		case KEY_DOWN:
-			term_line_down(input.terminals[input.current_terminal]);
-			return 1;
-		}
-	}
-
-	if (input.kbd_state.search_state && ev->value) {
-		switch (ev->code) {
-			case KEY_UP:
+	if (term_is_active(terminal)) {
+		if (input.kbd_state.shift_state && ev->value) {
+			switch (ev->code) {
+			case KEY_PAGEUP:
 				term_page_up(input.terminals[input.current_terminal]);
 				return 1;
-			case KEY_DOWN:
+			case KEY_PAGEDOWN:
 				term_page_down(input.terminals[input.current_terminal]);
 				return 1;
+			case KEY_UP:
+				term_line_up(input.terminals[input.current_terminal]);
+				return 1;
+			case KEY_DOWN:
+				term_line_down(input.terminals[input.current_terminal]);
+				return 1;
+			}
+		}
+
+		if (input.kbd_state.search_state && ev->value) {
+			switch (ev->code) {
+				case KEY_UP:
+					term_page_up(input.terminals[input.current_terminal]);
+					return 1;
+				case KEY_DOWN:
+					term_page_down(input.terminals[input.current_terminal]);
+					return 1;
+			}
 		}
 	}
 
 	if (input.kbd_state.alt_state && input.kbd_state.control_state && ev->value) {
 		if (ev->code == KEY_F1) {
-			terminal = input.terminals[input.current_terminal];
 			if (term_is_active(terminal)) {
 				input_ungrab();
 				terminal->active = false;
@@ -170,7 +173,6 @@ static int input_special_key(struct input_key_event *ev)
 				kLibCrosServicePath,
 				kLibCrosServiceInterface,
 				kReleaseDisplayOwnership);
-			terminal = input.terminals[input.current_terminal];
 			if (term_is_active(terminal))
 					terminal->active = false;
 			input.current_terminal = ev->code - KEY_F2;
@@ -500,27 +502,24 @@ int input_run(bool standalone)
 		if (term_exception(terminal, &exception_set))
 			return -1;
 
-		if (term_is_active(terminal)) {
-			struct input_key_event *event;
-			event = input_get_event(&read_set, &exception_set);
-			if (event) {
-				if (!input_special_key(event) && event->value) {
-					uint32_t keysym, unicode;
-					// current_terminal can possibly change during
-					// execution of input_special_key
-					terminal = input.terminals[input.current_terminal];
-					if (term_is_active(terminal)) {
-						// Only report user activity when the terminal is active
-						report_user_activity(USER_ACTIVITY_OTHER);
-						input_get_keysym_and_unicode(
-							event, &keysym, &unicode);
-						term_key_event(terminal,
-								keysym, unicode);
-					}
+		struct input_key_event *event;
+		event = input_get_event(&read_set, &exception_set);
+		if (event) {
+			if (!input_special_key(event) && event->value) {
+				uint32_t keysym, unicode;
+				// current_terminal can possibly change during
+				// execution of input_special_key
+				terminal = input.terminals[input.current_terminal];
+				if (term_is_active(terminal)) {
+					// Only report user activity when the terminal is active
+					report_user_activity(USER_ACTIVITY_OTHER);
+					input_get_keysym_and_unicode(
+						event, &keysym, &unicode);
+					term_key_event(terminal,
+							keysym, unicode);
 				}
-
-				input_put_event(event);
 			}
+			input_put_event(event);
 		}
 
 		term_dispatch_io(terminal, &read_set);