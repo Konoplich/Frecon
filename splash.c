3b67c9d01255c96083ba9ac56d8e99c61ecd30c0
David Sodman <dsodman@chromium.org>
2015-02-18 21:36:38 -0800

    End splash with a final clear to smooth transition to chrome
    End the splash with a final clear to help smooth the transition
    to Chrome.  Also, add a delay in the non devmode case to help
    make the transition smooth when in verified mode as well.  This
    change also increases the delay to 8 seconds to allow chrome
    enough time to draw itself above the splash.
    
    BUG=chromium:455605
    TEST=verified on link
    
    Change-Id: Ib0086e4c36df7b2d5c8fd78c027bfa4b71cca773

--- splash.c
+++ splash.c
@@ -22,7 +22,7 @@
 
 #define  MAX_SPLASH_IMAGES      (30)
 #define  FILENAME_LENGTH        (100)
-#define  MAX_SPLASH_WAITTIME    (5)
+#define  MAX_SPLASH_WAITTIME    (8)
 
 typedef union {
 	uint32_t  *as_pixels;
@@ -302,6 +302,7 @@ int splash_run(splash_t* splash, dbus_t** dbus)
 			}
 			last_show_ms = now_ms;
 		}
+		splash_clear_screen(splash, video_buffer);
 		video_unlock(splash->video);
 
 		do {
@@ -332,6 +333,13 @@ int splash_run(splash_t* splash, dbus_t** dbus)
 			} else {
 				LOG(ERROR, "unable to open drm_master_relax");
 			}
+		} else {
+			/*
+			 * Below, we will wait for Chrome to appear above the splash
+			 * image.  If we are not in dev mode, wait and then exit
+			 */
+			sleep(MAX_SPLASH_WAITTIME);
+			exit(EXIT_SUCCESS);
 		}
 	}
 
