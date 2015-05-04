b514b453c771e33aad29894a4c21de30f2d15743
David Sodman <dsodman@chromium.org>
2015-05-04 11:19:19 -0700

    Add terminal properties to splash screen
    In order to support text display and input on the splash screen for
    powerwash and dev/normal transitions, the splash screen needs to be
    attached to a terminal.  This change creates a terminal for the splash
    screen and directs the name of the pts device to stdout so that
    the scripts that launch the terminal can access the terminal.
    
    BUG=chromium:450400
    TEST=test on link with looping splash screen
    
    Change-Id: I9ab746f67627a8258bf87920f3c462ef4341ff10

--- term.c
+++ term.c
@@ -31,6 +31,15 @@ struct term {
 	uint32_t *dst_image;
 };
 
+struct _terminal_t {
+	uint32_t     background;
+	video_t     *video;
+	dbus_t      *dbus;
+	struct term *term;
+	bool         active;
+	char        **exec;
+};
+
 
 static char *interactive_cmd_line[] = {
 	"/sbin/agetty",
@@ -64,12 +73,24 @@ static int term_draw_cell(struct tsm_screen *screen, uint32_t id,
 {
 	terminal_t *terminal = (terminal_t*)data;
 	uint32_t front_color, back_color;
+	uint8_t br, bb, bg;
+	uint32_t Y;
 
 	if (age && terminal->term->age && age <= terminal->term->age)
 		return 0;
 
-	front_color = (attr->fr << 16) | (attr->fg << 8) | attr->fb;
-	back_color = (attr->br << 16) | (attr->bg << 8) | attr->bb;
+	br = (terminal->background >> 16) & 0xFF;
+	bg = (terminal->background >> 8) & 0xFF;
+	bb = (terminal->background) & 0xFF;
+	Y = (3*br + bb + 4*bg) >> 3;
+
+	if (Y > 128) {
+		front_color = 0;
+		back_color = terminal->background;
+	} else {
+		front_color = (attr->fr << 16) | (attr->fg << 8) | attr->fb;
+		back_color = terminal->background;
+	}
 
 	if (attr->inverse) {
 		uint32_t tmp = front_color;
@@ -161,7 +182,7 @@ static void log_tsm(void *data, const char *file, int line, const char *fn,
 	fprintf(stderr, "\n");
 }
 
-terminal_t* term_init(bool interactive)
+terminal_t* term_init(bool interactive, video_t* video)
 {
 	const int scrollback_size = 200;
 	uint32_t char_width, char_height;
@@ -172,7 +193,11 @@ terminal_t* term_init(bool interactive)
 	if (!new_terminal)
 		return NULL;
 
-	new_terminal->video = video_init();
+	if (video)
+		new_terminal->video = video;
+	else
+		new_terminal->video = video_init();
+
 	if (!new_terminal->video) {
 		term_close(new_terminal);
 		return NULL;
@@ -271,6 +296,16 @@ void term_activate(terminal_t* terminal)
 	terminal->active = true;
 	video_setmode(terminal->video);
 	term_redraw(terminal);
+}
+
+void term_deactivate(terminal_t* terminal)
+{
+	if (!terminal->active)
+		return;
+
+	input_ungrab();
+	terminal->active = false;
+	video_release(terminal->video);
 }
 
 void term_set_dbus(terminal_t *term, dbus_t* dbus)
@@ -383,4 +418,35 @@ void term_add_fd(terminal_t* terminal, fd_set* read_set, fd_set* exception_set)
 const char* term_get_ptsname(terminal_t* terminal)
 {
 	return ptsname(shl_pty_get_fd(terminal->term->pty));
+}
+
+void term_set_background(terminal_t* terminal, uint32_t bg)
+{
+	terminal->background = bg;
+}
+
+int term_show_image(terminal_t* terminal, image_t* image)
+{
+	return image_show(image, terminal->video);
+}
+
+void term_write_message(terminal_t* terminal, char* message)
+{
+	FILE *fp;
+
+	fp = fopen(term_get_ptsname(terminal), "w");
+	if (fp) {
+		fputs(message, fp);
+		fclose(fp);
+	}
+}
+
+void term_hide_cursor(terminal_t* terminal)
+{
+	term_write_message(terminal, "\033[?25l");
+}
+
+void term_show_cursor(terminal_t* terminal)
+{
+	term_write_message(terminal, "\033[?25h");
 }
