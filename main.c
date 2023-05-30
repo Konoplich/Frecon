6cd0733778b70a6a9052f97c6b533ded7503e572
Paz Zcharya <pazz@google.com>
2023-05-30 20:52:58 +0000

    frecon: move splash screen logic from main() to a function
    This step is a preparation for not triggering a splash screen.
    Currently, frecon always displays a splash screen, and the logic for
    that is all done in main.
    Move that logic to a new function called `display_splash_screen()`.
    Later, we'll add logic deciding whether to call this function or not
    based on a flag.
    
    BUG=None
    TEST=None
    
    Change-Id: I84ecc35acb0ab97c322fa81b31f30df0240c4875

--- main.c
+++ main.c
@@ -294,20 +294,105 @@ static void legacy_print_resolution(int argc, char* argv[])
 	}
 }
 
-int main(int argc, char* argv[])
+static int display_splash_screen(int argc, char* argv[])
 {
 	int ret;
 	int c;
 	int pts_fd;
-	unsigned vt;
 	int32_t x, y;
 	splash_t* splash;
 	drm_t* drm;
 
+	pts_fd =  posix_openpt(O_RDWR | O_NOCTTY | O_CLOEXEC | O_NONBLOCK);
+
+	drm_set(drm = drm_scan());
+
+	splash = splash_init(pts_fd);
+	if (splash == NULL) {
+		LOG(ERROR, "Splash init failed.");
+		return EXIT_FAILURE;
+	}
+
+	/* These flags can be only processed after splash object has been created. */
+	optind = 1;
+	for (;;) {
+		c = getopt_long(argc, argv, "", command_options, NULL);
+
+		if (c == -1)
+			break;
+
+		switch (c) {
+			case FLAG_CLEAR:
+				splash_set_clear(splash, strtoul(optarg, NULL, 0));
+				break;
+
+			case FLAG_FRAME_INTERVAL:
+				splash_set_default_duration(splash, strtoul(optarg, NULL, 0));
+				break;
+
+			case FLAG_IMAGE:
+				if (!splash_is_hires(splash))
+					splash_add_image(splash, optarg);
+				break;
+
+			case FLAG_IMAGE_HIRES:
+				if (splash_is_hires(splash))
+					splash_add_image(splash, optarg);
+				break;
+
+			case FLAG_LOOP_COUNT:
+				splash_set_loop_count(splash, strtoul(optarg, NULL, 0));
+				break;
+
+			case FLAG_LOOP_START:
+				splash_set_loop_start(splash, strtoul(optarg, NULL, 0));
+				break;
+
+			case FLAG_LOOP_INTERVAL:
+				splash_set_loop_duration(splash, strtoul(optarg, NULL, 0));
+				break;
+
+			case FLAG_LOOP_OFFSET:
+				parse_offset(optarg, &x, &y);
+				splash_set_loop_offset(splash, x, y);
+				break;
+
+			case FLAG_OFFSET:
+				parse_offset(optarg, &x, &y);
+				splash_set_offset(splash, x, y);
+				break;
+
+			case FLAG_SCALE:
+				splash_set_scale(splash, strtoul(optarg, NULL, 0));
+				break;
+		}
+	}
+
+	for (int i = optind; i < argc; i++)
+		splash_add_image(splash, argv[i]);
+
+	if (drm && splash_num_images(splash) > 0) {
+		ret = splash_run(splash);
+		if (ret) {
+			LOG(ERROR, "Splash_run failed: %d.", ret);
+			if (!command_flags.daemon)
+				return EXIT_FAILURE;
+		}
+	}
+
+	splash_destroy(splash);
+	return EXIT_SUCCESS;
+}
+
+int main(int argc, char* argv[])
+{
+	int ret;
+	int c;
+	unsigned vt;
+
 	legacy_print_resolution(argc, argv);
 
 	fix_stdio();
-	pts_fd =  posix_openpt(O_RDWR | O_NOCTTY | O_CLOEXEC | O_NONBLOCK);
 
 	optind = 1;
 	opterr = 1;
@@ -401,14 +486,6 @@ int main(int argc, char* argv[])
 		return EXIT_FAILURE;
 	}
 
-	drm_set(drm = drm_scan());
-
-	splash = splash_init(pts_fd);
-	if (splash == NULL) {
-		LOG(ERROR, "Splash init failed.");
-		return EXIT_FAILURE;
-	}
-
 	if (command_flags.pre_create_vts) {
 		for (unsigned vt = command_flags.enable_vt1 ? TERM_SPLASH_TERMINAL : 1;
 		     vt < (command_flags.enable_vts ? term_num_terminals : 1); vt++) {
@@ -423,74 +500,11 @@ int main(int argc, char* argv[])
 	if (command_flags.daemon && command_flags.pre_create_vts)
 		daemon_exit_code(EXIT_SUCCESS);
 
-	/* These flags can be only processed after splash object has been created. */
-	optind = 1;
-	for (;;) {
-		c = getopt_long(argc, argv, "", command_options, NULL);
-
-		if (c == -1)
-			break;
-
-		switch (c) {
-			case FLAG_CLEAR:
-				splash_set_clear(splash, strtoul(optarg, NULL, 0));
-				break;
-
-			case FLAG_FRAME_INTERVAL:
-				splash_set_default_duration(splash, strtoul(optarg, NULL, 0));
-				break;
-
-			case FLAG_IMAGE:
-				if (!splash_is_hires(splash))
-					splash_add_image(splash, optarg);
-				break;
-
-			case FLAG_IMAGE_HIRES:
-				if (splash_is_hires(splash))
-					splash_add_image(splash, optarg);
-				break;
-
-			case FLAG_LOOP_COUNT:
-				splash_set_loop_count(splash, strtoul(optarg, NULL, 0));
-				break;
-
-			case FLAG_LOOP_START:
-				splash_set_loop_start(splash, strtoul(optarg, NULL, 0));
-				break;
-
-			case FLAG_LOOP_INTERVAL:
-				splash_set_loop_duration(splash, strtoul(optarg, NULL, 0));
-				break;
-
-			case FLAG_LOOP_OFFSET:
-				parse_offset(optarg, &x, &y);
-				splash_set_loop_offset(splash, x, y);
-				break;
-
-			case FLAG_OFFSET:
-				parse_offset(optarg, &x, &y);
-				splash_set_offset(splash, x, y);
-				break;
-
-			case FLAG_SCALE:
-				splash_set_scale(splash, strtoul(optarg, NULL, 0));
-				break;
-		}
+	ret = display_splash_screen(argc, argv);
+	if (ret) {
+		LOG(ERROR, "Splash screen error.");
+		return EXIT_FAILURE;
 	}
-
-	for (int i = optind; i < argc; i++)
-		splash_add_image(splash, argv[i]);
-
-	if (drm && splash_num_images(splash) > 0) {
-		ret = splash_run(splash);
-		if (ret) {
-			LOG(ERROR, "Splash_run failed: %d.", ret);
-			if (!command_flags.daemon)
-				return EXIT_FAILURE;
-		}
-	}
-
-	splash_destroy(splash);
 
 	if (command_flags.splash_only)
 		goto main_done;
